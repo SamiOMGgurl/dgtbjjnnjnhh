37ca0b098c893674bc6aa011a3394378ace0f564
SamiOMGgurl <83353299+SamiOMGgurl@users.noreply.github.com>
2022-09-16 14:35:22 +0400

    Update quiz_game_page.js

--- quiz_game_page.js
+++ quiz_game_page.js
@@ -17,7 +17,8 @@ document.getElementById("player1_question").innerHTML = "Question Turn -" + play
 document.getElementById("player2_answer").innerHTML = "Answer Turn -" + player2_name ;
 
 
-
+question_turn = "player1";
+answer_turn = "player2";
 
 
 function send()
@@ -37,8 +38,7 @@ document.getElementById("number2").value = "";
 
 
 
-question_turn = "player1";
-answer_turn = "player2";
+
 
 function check()
 {
@@ -50,13 +50,13 @@ function check()
            player1_score = player1_score +1;
             document.getElementById("player1_score").innerHTML = player1_score;
         }
-    }
+ 
     else
     {
         player2_score = player2_score +1;
         document.getElementById("player2_score").innerHTML = player2_score;
     }
-
+   }
     if (question_turn == "player1")
     {
      question_turn = "player2";
@@ -68,7 +68,17 @@ else
     question_turn = "player1";
     document.getElementById("player_question").innerHTML = "Question Turn -" + player1_name; 
 }
+if (answer_turn== "player1")
+    {
+     answer_turn = "player2";
+        document.getElementById("player_question").innerHTML = "Answer Turn - " + player2_name;
+    }
 
+else
+{
+    answer_turn = "player1";
+    document.getElementById("player_question").innerHTML = "Answer Turn -" + player1_name; 
+}
   
 
 
